12b934b4f178271d1bf0177a9f36b71e00b622ba
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-12-23 15:17:16 +0200

    update to new compile script

--- test-stuff/compile-etcher.sh
+++ test-stuff/compile-etcher.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+#check if CPU is ARM (32bit or 64bit)
+ARCH=$(uname -m)
+if [ "${ARCH}" != "aarch64" ]; then
+  
+  #check if CPU is ARM 32bit
+  ARCH=$(uname -m)
+  if [ "${ARCH}" != "armv7l" "armhf" ]; then
+   echo "your cpu architecture isn't ARM, this script is only intended for arm cpu's"
+   echo "exiting in 5 seconds..."
+   sleep 5; exit
+ else
+  echo "CPU architecture is ARM 32bit"
+  ARCH1="32bit ARM"
+ fi
+
+else
+  echo "CPU architecture is ARM 64bit"
+  ARCH1="ARM 64bit"
+fi
+
+
 #ask if user wants to continue
 clear
-echo -n "this script will compile and package etcher v1.5.112 for arm32/64, this will take around 30 minutes on a pi 4 on stock clock speed and consume almost all memory and cpu during some parts. a fan or at least a heatsink is recommended for the pi 4. Do you want to continue? [y/n] "
+echo this script will compile and package etcher for $ARCH1
+echo "this will take around 30 minutes on a pi 4 on stock clock speed and consume almost all memory and cpu during some parts."
+echo "a fan or at least a heatsink is recommended for the pi 4. Do you want to continue? [y/n]"
 read answer
 if [ "$answer" == n ];then
  echo "exiting in 5 seconds"
@@ -14,22 +37,23 @@ if [ "$answer" == y ];then
  sleep 1
 fi
 
-
 #ask for version to compile  
 echo "Enter etcher version to compile (e.g 1.5.112): "  
 read VERSION  
 
+#if no user input, set to v1.5.112 (doesn't work yet)
+if ["$VERSION" == ""];then
+ VERSION=1.5.112
+fi
 
-#if ["$VERSION" == ""];then
-# $VERSION=1.5.112
-#fi
-
+#tell user what version will be compiled
 echo "version compiled will be v$VERSION"
 
-echo -n "install dependencies (required unless already installed (e.g running script 2nd time) [y/n]"
+#ask user to install dependencies and install/skip 
+echo -n "install dependencies (required unless already installed (e.g running script 2nd time)), **answering no isn't recommended** [y/n]"
  read answer
  if [ "$answer" == n ];then
- echo "dependencies won't be installed. BEWARE: compiling and packaging will fail unless already installed"
+ echo "dependencies won't be installed. BEWARE: compiling and packaging will fail unless dependencies are already installed"
 fi
 if [ "$answer" == y ];then
  cd ~/Downloads
@@ -44,20 +68,23 @@ if [ "$answer" == y ];then
  clear
 fi
 
+#clone etcher repo and checkout realease to be compiled
 echo "$(tput setaf 3)cloning etcher repo and checking out realease (your input)$(tput sgr 0)"
-cd ~/Downloads
 git clone --recursive https://github.com/balena-io/etcher
 cd etcher
-git checkout v$VERSION
+git checkout $VERSION
 
+#install requirements
 echo "$(tput setaf 3)installing requirements...$(tput sgr 0)"
 pip install -r requirements.txt
 
+#set up and install npm modules
 echo "$(tput setaf 3)setting up and installing NPM modules...$(tput sgr 0)"
 make electron-develop
 sleep 4
 clear
 
+#ask user to run a test of etcher to see if compile worked and do it/skip it
 echo -n "do you want to run a test of etcher to see if compile worked? [y/n] "
 read answer
 if [ "$answer" == y ];then
@@ -69,6 +96,7 @@ if [ "$answer" == n ];then
  echo "$(tput setaf 3)continuing...$(tput sgr 0)"
 fi
 
+#build and pacakge etcher to a .deb package
 echo "$(tput setaf 3)building & packaging etcher into a .deb file...$(tput sgr 0)"
 sed -i 's/tiffutil/#tiffutil/g' Makefile 
 sed -i 's/TARGETS="deb rpm appimage"/TARGETS="deb"/g' scripts/resin/electron/build.sh

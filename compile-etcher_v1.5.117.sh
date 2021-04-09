91505727fe81a9a886eb072f55f880a8ea1dd959
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-04-09 16:54:46 +0300

    Update compile-etcher-v1.5.117.sh

--- compile-etcher_v1.5.117.sh
+++ compile-etcher_v1.5.117.sh
@@ -1,144 +1,220 @@
 #!/bin/bash
 
-ETCHER_VER="v1.5.117"
+###instructions from here: https://github.com/futurejones/balena-etcher-arm/blob/master/etcher-build/BUILD.md###
 
-function install-node-from-nodesource() {
-  while true; do
-    read -p "nvm failed to install node.js and npm, would you like to install it from the nodesource apt repo? WARNING: it might break system permissions! (y/n)?" answer
-    if [[ "$answer" =~ [yY] ]]; then
-      curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-      sudo apt install -y nodejs || echo "apt failed to install node.js!" && sleep 1 && exit 1
-      break
-    elif [[ "$answer" =~ [nN] ]]; then
-      echo "OK"
-      sleep 0.5
-      break
+
+#variables
+DEPENDS="git python gcc g++ make libx11-dev libxkbfile-dev fakeroot rpm libsecret-1-dev jq python2.7-dev python-pip python-setuptools libudev-dev ruby-dev"
+
+###functions###
+function error() {
+    echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+    exit 1
+}
+
+function warning() {
+    echo -e "$(tput setaf 3)$(tput bold)$1$(tput sgr 0)"
+    sleep 5
+}
+
+function warning-sleep() {
+    #USAGE:
+    #warning-sleep "warning: something happened" 2
+    #
+    #$1 is the text
+    #$2 is the amount of seconds to sleep
+    echo -e "$(tput setaf 3)$(tput bold)$1$(tput sgr 0)"
+    sleep $2
+}
+
+function pkg-manage() {
+    #USAGE:
+    #usage: pkg-manage install "package1 package2 package3"
+    #pkg-manage uninstall "package1 package2 package3"
+    #pkg-manage check "packag1 package2 package3"
+    #pkg-manage clean
+    #
+    #$1 is the operation: install or uninstall
+    #$2 is the packages to operate on.
+    if [[ "$1" == "install" ]]; then
+        TOINSTALL="$(dpkg -l $2 2>&1 | awk '{if (/^D|^\||^\+/) {next} else if(/^dpkg-query:/) { print $6} else if(!/^[hi]i/) {print $2}}' | tr '\n' ' ')"
+        sudo apt -f -y install $TOINSTALL || sudo apt -f -y install "$TOINSTALL"
+    elif [[ "$1" == "uninstall" ]]; then
+        sudo apt purge $2 -y
+    elif [[ "$1" == "check" ]]; then
+        TOINSTALL="$(dpkg -l $2 2>&1 | awk '{if (/^D|^\||^\+/) {next} else if(/^dpkg-query:/) { print $6} else if(!/^[hi]i/) {print $2}}' | tr '\n' ' ')"  
+    elif [[ "$1" == "clean" ]]; then
+        sudo apt clean
+        sudo apt autoremove -y
+        sudo apt autoclean
     else
-      echo "invalid answer '$answer'! please try again."
+        error "operation not specified!"
     fi
-  done
+}
+
+function install-nvm() {
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
 function install-node() {
-  echo "please run: wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
-  echo "and then run this script again"
-  exit
-  #curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash || error "Failed to install nvm!"
-  #export NVM_DIR="$HOME/.nvm"
-  #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  #if [[ -f "$HOME/.bashrc" ]]; then
-  #  source ~/.bashrc
-  #fi
-  #if [[ "$ARCH" == "armhf" ]]; then
-  #  sed -i 's/^  nvm_echo "${NVM_ARCH}"/  NVM_ARCH=armv7l ; nvm_echo "${NVM_ARCH}"/g' "$NVM_DIR/nvm.sh"
-  #fi
-  #chmod +x $NVM_DIR/nvm.sh
-  #nvm install v15.12.0 --latest-npm || install-node-from-nodesource || echo "failed to install node.js and npm!" && sleep 1 && exit 1
+    warning-sleep "WARNING: using this method to install Node.js is known to BREAK SYSTEM PERMISSIONS!" 2
+    if ! command -v curl >/dev/null ; then
+        echo -e "\033[0;31mcurl: command not found.\n\e[39m You need to install curl first. If you are on a debian system, this command should install it: \e[4msudo apt install curl\e[0m"
+        exit 1
+    fi
+    curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+    sudo apt install -y nodejs
 }
 
-function compile-etcher() {
-  cd ~/Downloads
-  echo "$(tput setaf 3)installing dependencies...$(tput sgr 0)"
-  sudo apt install -y curl git python gcc g++ make libx11-dev libxkbfile-dev fakeroot rpm libsecret-1-dev jq python2.7-dev python-pip python-setuptools libudev-dev jq
-  sudo apt install -y ruby-dev
-  sudo gem install fpm -v 1.10.2 --no-document
+function install-depends() {
+    pkg-manage install "$DEPENDS" || error "Failed to install dependencies!"
+    sudo gem install fpm -v 1.10.2 --no-document #note: must be v1.10.2 NOT v1.11.0
+    sleep 1
+    clear -x
+    if ! command -v node >/dev/null ; then
+        while true; do
+            echo -ne "Node.js and npm aren't installed but required,\ndo you want to install them with nvm (recommended) or with apt from the nodesource repo (NOT RECOMENDED!, KNOWN TO BREAK SYSTEM PERMISSIONS!) [nvm/nodesource]?"
+            read answer
+            if [[ "$answer" == "nvm" ]]; then
+                install-nvm || error "Failed to install nvm!"
+                nvm install node --latest-npm || error "nvm failed to install node.js and npm!"
+                npm install -g npm@6.14.8 || error "Failed to install npm 6.14.8 (required for building etcher)!"
+                break
+            elif [[ "$answer" == "nodesource" ]]; then
+                warning-sleep "WARNING: using THIS method to install Node.js is KNOWN to BREAK SYSTEM PERMISSIONS!" 2
+                while true; do
+                    echo -ne "are you sure you want to continue [+/n]?"
+                    read answer
+                    if [[ "$answer" == "+" ]]; then
+                        warning-sleep "BEWARE: I'm not responsible if your system breaks as the result of using this method!\npress [CTRL+C] in the next 10 seconds to exit." 10
+                        install-node
+                        break
+                    elif [[ "$answer" =~ [nNnoNonONO] ]]; then
+                        echo -e "OK, you made the better choice :)\nrun the script again."
+                        sleep 0.5
+                        exit 0
+                    else
+                        warning-sleep "inalid answer \"$answer\"! please try again" 0
+                    fi
+                done
+                break
+            else
+                warning-sleep "invalid answer \"$answer\"! please try again" 0
+            fi
+        done
+    fi
 
-  if ! command -v curl >/dev/null ; then
-    echo -e "\033[0;31mcurl: command not found.\e[39mYou need to install curl first. If you are on a debian system, this command should install it:\e[4msudo apt install curl\e[0m"
-    exit 1
-  fi
-  #Checking if using armv6
-  if [ ! -z "$(cat /proc/cpuinfo | grep ARMv6)" ];then
-    echo -e "\e[31marmv6 CPU not supported!\e[0m"
-    exit 1
-  fi
-  #check that OS arch is armhf
-  ARCH="$(uname -m)"
-  if [[ "$ARCH" == "aarch64" ]] || [[ "$ARCH" == "arm64" ]] || [[ "$ARCH" == "armv7l" ]] || [[ "$ARCH" == "armhf" ]]; then
+}
+
+
+#####things start to get done here#####
+#check that system is compatible
+#is CPU armv6?
+if [ ! -z "$(cat /proc/cpuinfo | grep ARMv6)" ];then
+  error "armv6 CPU not supported!"
+fi
+
+#check if arch is arm
+ARCH="$(uname -m)"
+if [[ "$ARCH" == "armv7l" ]] || [[ "$ARCH" == "armhf" ]] || [[ "$ARCH" == "arm64" ]] || [[ "$ARCH" == "aarch64" ]]; then
     if [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 64)" ];then
         ARCH="arm64"
     elif [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 32)" ];then
         ARCH="armhf"
     else
-        echo -e "$(tput setaf 1)$(tput bold)Can't detect OS architecture! something is very wrong!$(tput sgr 0)"
-        exit 1
+        error "Can't detect OS architecture! something is very wrong!"
     fi
+else
+    error "Unsuported architecture! this script is only intended to be run on linux arm devices."
+fi
+while true; do
+  echo -ne "This script will compile and package etcher for arm32/64,\nthis will take around 30 minutes on a pi 4b 4gb on stock clock speeds booting from a samsung evo+ 32gb sd card and consume almost all memory and cpu.\nA fan or at least a heatsink is recommended.\nWARNING: THIS SCRIPT WON'T UNINSTALL THE DEPENDENCIES IT INSTALLS!\nDo you want to continue [y/n]? "
+  sleep 0.5
+  read answer
+  if [[ "$answer" =~ [nN] ]]; then
+    echo "exiting in 2 seconds..."
+    sleep 2
+    exit
+    break #in case exit fails (very unlikely)
+  elif [[ "$answer" =~ [yY] ]]; then
+    true
+    break
   else
-    echo -e "$(tput setaf 1)$(tput bold)ERROR: '$ARCH' isn't a supported architecture!\nthis script is only intended to run on arm32 and arm64!$(tput sgr 0)"
-    exit 1
+    echo "invalid answer '$answer'! please try again."
   fi
+done
 
-  if ! command -v node >/dev/null ; then
-    echo "Node.js and npm are needed."
-    echo -n "do you want to install them using 'nvm' (recommended) or the nodesource repo (known to break system permissions!) [nvm/nodesource/already-installed]?"
-    read answer
-    if [[ $answer == "nvm" ]]; then
-      install-node
-    elif [[ $answer == "nodesource" ]]; then
-      install-node-from-nodesource
-    elif [[ $answer == "already-installed" ]]; then
-      true
+#install all the dependencies
+install-depends
+sleep 0.5
+clear -x
+#get directory to build etcher in
+while true; do
+    echo -ne "Enter full path to the directory where you want to build etcher:\n"
+    read -r DIR
+    if [ ! -d $DIR ]; then
+        echo -e "\e[1mdirectory does not exist, please try again\e[0m"
     else
-      echo "invalid answer."
-      exit 1
+        echo -e "\e[1mEtcher will be built and packaged here: $DIR\e[0m"
+        break
     fi
-  fi
-
-  sleep 4
-  clear -x
-  echo "$(tput setaf 3)cloning etcher repo and checking out realease ($ETCHER_VER)$(tput sgr 0)"
-  cd ~/Downloads
-  git clone --recursive https://github.com/balena-io/etcher
-  cd etcher
-  git checkout $ETCHER_VER
-  echo "$(tput setaf 3)installing requirements...$(tput sgr 0)"
-  pip install -r requirements.txt
-  echo "$(tput setaf 3)setting up and installing NPM modules...$(tput sgr 0)"
-  make electron-develop
-  sleep 4
-  clear -x
-  while true; do
-    echo -n "do you want to run a test of etcher to see if compile worked [y/n]? "
+done
+cd "$DIR"
+#clone the etcher repo
+git clone --recursive https://github.com/balena-io/etcher
+cd etcher
+clear -x
+#get version of etcher to compile and check it out (using git)
+#read -p "Enter Etcher version to compile (newest: v1.5.117)" ETCHER_VER
+#if [[ $ETCHER_VER == v* ]]; then
+#    git checkout $ETCHER_VER || error "Failed to checkout version \"$ETCHER_VER\"!"
+#else
+#    git checkout v$ETCHER_VER || error "Failed to checkout version \"v$ETCHER_VER\"!"
+#fi
+git checkout v1.5.117
+#install requirements (with pip)
+pip install -r requirements.txt
+#setup and install NPM modules
+make electron-develop
+#ask if you want to test if the build worked before packaging it (using npm start)
+while true; do
+    echo -n "do you want to run a test of etcher to see if compilation worked [y/n]? "
     read answer
     if [[ "$answer" =~ [yY] ]]; then
-      echo -e "$(tput setaf 3)close etcher window to continue to packaging.\nwaiting for 5 seconds...$(tput sgr 0)"
-      sleep 5
-      npm start
-      break
+        echo -ne "$(tput setaf 3)close etcher window to continue to packaging.$(tput sgr 0)\nwaiting for 5 seconds"
+        #fancy "progress bar"
+        while true;do echo -n .;sleep 1;done &
+        sleep 5
+        kill $!; trap 'kill $!' SIGTERM
+        echo -e "\nStarting Etcher..."
+        npm start || error "Etcher failed to start!"
+        break
     elif [[ "$answer" =~ [nN] ]];then
       echo "$(tput setaf 3)continuing...$(tput sgr 0)"
       break
     else
       echo "invalid option '$answer', please try again."
     fi
-  done
-  echo "$(tput setaf 3)building & packaging etcher into a .deb file...$(tput sgr 0)"
-  sed -i 's/tiffutil/#tiffutil/g' Makefile 
-  sed -i 's/TARGETS="deb rpm appimage"/TARGETS="deb"/g' scripts/resin/electron/build.sh
-  USE_SYSTEM_FPM="true" make electron-build
-  sleep 5
-  clear -x
-  echo ".deb file will be in ~/Downloads/etcher/dist/"
-}
-
-clear -x
-while true; do
-  echo -ne "this script will compile and package etcher $ETCHER_VER for arm32/64, this will take around 30 minutes on a pi 4 on stock clock speed and consume almost all    memory and cpu. a fan or at least a heatsink is recommended for the pi 4.\nWARNING: THIS SCRIPT WON'T UNINSTALL THE DEPENDENCIES IT INSTALLS!\nDo you want to continue [y/n]? "
-  sleep 0.5
-  read answer
-  if [[ "$answer" =~ [nN] ]]; then
-    echo "exiting in 5 seconds"
-    sleep 5
-    exit
-    break #in case exit fails (very unlikely)
-  elif [[ "$answer" =~ [yY] ]]; then
-    compile-etcher 
-    echo "$(tput setaf 3)exiting in 10 seconds... $(tput sgr 0)"
-    sleep 10
-    exit
-    break #in case exit fails (very unlikely)
-  else
-    echo "invalid answer '$answer, please try again"
-  fi
 done
+
+##patch build files##
+# disable tiffutil in the Makefile as this is a Mac only app and will cause the build to fail
+sed -i 's/tiffutil/#tiffutil/g' Makefile  || error "Failed to patch Makefile!"
+# restrict output to .deb package only to save build time
+sed -i 's/TARGETS="deb rpm appimage"/TARGETS="deb"/g' scripts/resin/electron/build.sh || error "Failed to patch 'build.sh' script"
+##compile and build etcher##
+# use USE_SYSTEM_FPM="true" to force the use of the installed FPM version
+USE_SYSTEM_FPM="true" make electron-build  || error "Failed to run \"USE_SYSTEM_FPM="true" make electron-buil\"!"
+
+#get the finished deb's name
+DEBNAME="$(basename $DIR/etcher/dist/balena-etcher-electron*.deb)"
+sleep 1
+clear -x
+echo "The deb is in \"$DIR/etcher/dist/$DEBNAME\"."
+echo "install it with \"sudo apt -f install .$DIR/etcher/dist/$DEBNAME\""
+sleep 2
+echo "DONE!"
+exit 0
